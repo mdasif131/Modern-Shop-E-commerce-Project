a2d1a936d1f2583955c2c69297d0685101370c48
mdasif131 <asifmdasifchy@gmail.com>
2025-09-29 20:54:07 +0600

    setup prject

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './Container'
+
+const Footer = () => {
+  return (
+    <Container>
+      <div>Footer</div>
+    </Container>
+  );
+}
+
+export default Footer
